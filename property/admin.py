f1de686b31c4642ebd35857f0ca63a5b29c69d05
Бирюков Максим <maxandix@gmail.com>
2020-08-01 16:32:46 +0300

    add Complaint and likes

--- property/admin.py
+++ property/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Flat
+from .models import Flat, Complaint
+
 
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ['town', 'address', 'owner']
@@ -8,5 +9,12 @@ class FlatAdmin(admin.ModelAdmin):
     list_display = ('address', 'price', 'new_building', 'construction_year', 'town')
     list_editable = ['new_building']
     list_filter = ['new_building', 'rooms_number', 'has_balcony']
+    raw_id_fields = ['liked_by']
+
+
+class Complaintdmin(admin.ModelAdmin):
+    raw_id_fields = ['author', 'flat']
+
 
 admin.site.register(Flat, FlatAdmin)
+admin.site.register(Complaint, Complaintdmin)
--- property/models.py
+++ property/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 class Flat(models.Model):
     owner = models.CharField("ФИО владельца", max_length=200)
     owners_phonenumber = models.CharField("Номер владельца", max_length=20)
     created_at = models.DateTimeField("Когда создано объявление", default=timezone.now, db_index=True)
-    
+
     description = models.TextField("Текст объявления", blank=True)
     price = models.IntegerField("Цена квартиры", db_index=True)
 
@@ -22,6 +23,13 @@ class Flat(models.Model):
     active = models.BooleanField("Активно-ли объявление", db_index=True)
     construction_year = models.IntegerField("Год постройки здания", null=True, blank=True, db_index=True)
     new_building = models.NullBooleanField("Новостройка", db_index=True)
+    liked_by = models.ManyToManyField(User, related_name="liked_flats", verbose_name='Кто лайкнул')
 
     def __str__(self):
         return f"{self.town}, {self.address} ({self.price}р.)"
+
+
+class Complaint(models.Model):
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL, verbose_name='Кто жаловался')
+    flat = models.ForeignKey(Flat, on_delete=models.CASCADE, verbose_name='Квартира, на которую жаловались')
+    description = models.TextField("Текст жалобы", blank=True)
